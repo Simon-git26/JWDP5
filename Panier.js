fba2f0352f4e9f48ea82169eba80c12ebc039188
Simon Balleux <simoncestmoi@hotmail.fr>
2021-04-01 12:34:58 +0200

    Affichage de produit mais pas comme il faut PAGE PANIER

--- Panier.js
+++ Panier.js
@@ -21,3 +21,34 @@ if (teddyArticleJson) {
 
 }
 
+
+
+/* Recuperer et afficher les données sur la page Web */
+
+
+const container = document.getElementById('products');
+let template = document.querySelector('template');
+
+for (const result of teddyArticle) {
+    let clone = document.importNode(template.content, true);
+    
+    let img = clone.querySelectorAll("img");
+    img[0].src = result.imageUrl;
+    console.log(img[0]);
+
+
+    
+    let div = clone.querySelectorAll("div");
+
+    div[3].textContent = result.name;
+    console.log(div[3]);
+
+
+    div[5].textContent = `${result.price / 100} €`;
+    console.log(div[5]);
+    
+    
+    container.appendChild(clone);
+}
+
+
