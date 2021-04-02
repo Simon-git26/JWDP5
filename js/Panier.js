1881c710315376ccf948a877c3ca63d415835e93
Simon Balleux <simoncestmoi@hotmail.fr>
2021-04-02 10:18:09 +0200

    Ajout quantiter panier et affichage

--- js/Panier.js
+++ js/Panier.js
@@ -28,9 +28,24 @@ for (const result of teddyArticle) {
     div[3].textContent = result.name;
     console.log(div[3]);
 
-
     div[5].textContent = `${result.price / 100} €`;
-    console.log(div[5]);
     
     container.appendChild(clone);
 }
+
+
+
+/* const container = document.getElementById('products');
+let template = document.querySelector('template');
+
+for (const result of teddyArticle) {
+    let clone = document.importNode(template.content, true);
+    
+    clone.querySelector('td:nth-child(1)').textContent = result.name
+
+    clone.querySelector('td:nth-child(2)').textContent = 1
+    clone.querySelector('td:nth-child(3)').textContent = `${result.price / 100} €`;
+    clone.querySelector('td:nth-child(4)').textContent = `${result.price * result.quantite / 100} €`;
+
+    container.appendChild(clone);
+} */
--- js/Produit.js
+++ js/Produit.js
@@ -40,7 +40,8 @@ findTeddy(id).then(response => {
 })
 
 
-cart2.onclick = () => {
+
+document.querySelector('.addToCart').addEventListener('click', () => {
 
     const articleStorage = localStorage.getItem('article');
     let articles = [];
@@ -52,4 +53,28 @@ cart2.onclick = () => {
     articles.push(teddyArticle);
     
     localStorage.setItem('article', JSON.stringify(articles));
-};
+});
+
+
+
+/* document.querySelector('.addToCart').addEventListener('click', (event) => {
+    event.preventDefault()
+    const target = event.target.parentElement.parentElement
+    const quantite = Number(target.querySelector('input[type="number"]').value)
+    const cart = localStorage.getItem('article') ? JSON.parse(localStorage.getItem('article')) : [];
+    const existe = cart.filter((item) => item._id === id)[0];
+    
+
+    if (existe) {
+        let index = cart.indexOf(existe)
+        let article = cart.splice(index, 1)[0];
+        existe.quantite += quantite
+        cart.push(existe)
+    } else {
+        teddyArticle.quantite = quantite
+        cart.push(teddyArticle)
+    }
+
+    cart.push(existe);
+    localStorage.setItem('article', JSON.stringify(cart));
+}); */
